a4506c651b2c3a423259027c01bddfffa848fe9b
ivaspasovskaa <138396617+ivaspasovskaa@users.noreply.github.com>
2023-07-03 00:57:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+        if(this.scrollY>350){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed('.auto-input',{
+        strings: ['student at FINKI.', "Front-End Developer."],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true,
+    });
+});
